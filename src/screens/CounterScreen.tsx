8c9b6a507afb61b0686476faf79f55a91e8a1edf
Vladimir Saldivar <vladimir.saldivar@morsco.com>
2023-01-15 09:34:22 -0600

    FCommit

---
+++ src/screens/CounterScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export const CounterScreen = () => {
+  return (
+    <View>
+        <Text>Contador: xxx</Text>
+    </View>
+  )
+}
