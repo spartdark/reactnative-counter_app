7b338a60e694b425081f2d333bf33cfd9cdf30bc
Vladimir Saldivar <vladimir.saldivar@morsco.com>
2023-01-15 21:08:59 -0600

    class 56 section 4

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -3,7 +3,9 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 export const CounterScreen = () => {
 
-    const [contador, setContador] = useState(10);
+    const [contador, setContador] = useState(0);
+
+
 
     return (
         <View
@@ -11,14 +13,23 @@ export const CounterScreen = () => {
             <Text style={myStyles.tittle}>Contador: {contador}</Text>
 
             <TouchableOpacity
+                style={myStyles.fabLocation}
                 onPress={() => setContador(contador + 1)}
             >
-                <View
-                    style={myStyles.buttonPlus}
-                ><Text>
+                <View style={myStyles.fab}>
+                    <Text style={myStyles.fabText}>
                         +1
-                    </Text></View>
+                    </Text>
+                </View>
+            </TouchableOpacity>
 
+            <TouchableOpacity
+                style={myStyles.fabLocationLeft}
+                onPress={() => setContador(contador - 1)}
+            >
+                <View style={myStyles.fab}>
+                    <Text style={myStyles.fabText}> -1 </Text>
+                </View>
             </TouchableOpacity>
 
         </View>
@@ -36,9 +47,29 @@ const myStyles = StyleSheet.create({
         position: 'relative',
         top: -15,
     },
-    buttonPlus: {
-        backgroundColor: 'red',
-        borderRadius: 100
+    fabLocation: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25
+    },
+    fabLocationLeft: {
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    }
+    ,
+    fab: {
+        backgroundColor: '#5cc9f5',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center'
     }
 
 })
